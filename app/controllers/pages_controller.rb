4e566941351e4129b01e64c9fc8456fd6c4cf135
Sadique <sadiquea346@gmail.com>
2019-12-19 16:45:51 +0500

    Commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 	def about
 		@title = 'About Us';
 		@content = 'We integrate global leaders in web development with
-		 passionate Asian talent to get a unique blend of Quality and Affordability.
+		 passionate   Asian talent to get a unique blend of Quality and Affordability.
 		 We are headquartered in California, and work consistent eastern and pacific 
 		 standard hours. We like ad hoc pairing as necessary, TDD, and working with 
 		 other agencies to make things happen. We contribute to open source projects 
